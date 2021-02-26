1b2f49a22db6780e9dbc1abf3ffc6157f780edc9
Matt Hart <mp.hart047@gamil.com>
2021-02-26 17:57:37 -0600

    Syntax_Suger 1.0 basic

---
+++ Syntax_Suger/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Syntax_Suger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var responce = (answer < 9)? " is less than nine.":
+                " is greater than or equal to nine.";
+            Console.WriteLine($"{ answer}{ responce}");
+
+
+        }
+    }
+}
